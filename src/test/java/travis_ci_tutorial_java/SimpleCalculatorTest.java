e066685ec24310f7563948f25f2222ca658d7501
Joseph Adrian <joseph.danger.adrian@gmail.com>
2021-12-12 16:23:08 -0500

    adding new test but for a method that doesn't exist yet

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,11 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	@Test
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(2,2),0);
+		assertEquals(calc.minus(2,3),-1);
+		assertEquals(calc.minus(2,0),2);
+	}
 }
